274864d10921cc06f3da5c43de18560de0d31d48
Don McLamb <mclamb.donald@gmail.com>
2016-06-04 15:19:09 -0400

    fixed bugs  added simpleschema, added collection2, removed events(restricted name) from template name

--- client/templateManagers/events/calendar.js
+++ client/templateManagers/events/calendar.js
@@ -2,7 +2,40 @@
 //   $('#events-calendar').fullCalendar();
 // });
 
+
+
+//These events use a different process of invoking by placing the action at the end as a arrow method
+Template.calendar.onCreated( () => {
+  let template = Template.instance();
+  template.subscribe('events');
+});
+
 //Arrrow funciton es6
-Template.events.onRendered( () =>{
-  $('#events-calendar').fullCallendar();
+Template.calendar.onRendered( () =>{
+  $('#events-calendar').fullCalendar({
+    events(start, end, timezone, callback){
+      let data = Events.find().fetch().map( (event)  => {
+        event.editable = !isPast( event.start );
+        return event;
+      });
+
+      if (data){
+        callback(data);
+      }
+    },
+    //MINE
+    eventRender(event, element){
+      element.find('.fc-content').html(
+        `<h4>${ event.title }</h4>
+        <p class="guest-count">${ event.guests } Guests</p>
+        <p class="type-${ event.type }">#${ event.type}</p>
+        `
+      );
+    }
+  });
+
+  Tracker.autorun( () => {
+    Events.find().fetch();
+    $( '#events-calendar').fullCalendar('refetchEvents')
+  });
 });
