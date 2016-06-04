abb87724869715acedc620635cfee7b5ff5a0524
Don McLamb <mclamb.donald@gmail.com>
2016-06-03 21:45:47 -0400

    setup and modded structure. created schema

---
+++ client/templateManagers/events/calendar.js
@@ -0,0 +1,8 @@
+// Template.events.onRendered({
+//   $('#events-calendar').fullCalendar();
+// });
+
+//Arrrow funciton es6
+Template.events.onRendered( () =>{
+  $('#events-calendar').fullCallendar();
+});
